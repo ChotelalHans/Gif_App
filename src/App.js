84a8735ab46a6ee858a9e8269720c744aa58cec9
ChotelalHans <tajeshwiraj9572@gmail.com>
2021-08-13 17:30:34 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Random from './components/Random';
+import Tag from './components/Tag';
+
+function App() {
+  return (
+    <div className="App">
+     
+         <h1> Random GIF Application</h1>  
+        <div className="main-container">
+           <Random/>
+           
+          <Tag/>
+        </div>
+       
+    </div> 
+  );
+} 
+
+export default App;
+  
